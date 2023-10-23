9408dce342c1c099b2b300159574817664c6318f
ahbren <82262463+ahbren@users.noreply.github.com>
2023-10-23 20:06:39 +0800

    [FIX] conflict

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,7 @@ import Playground from "views/Playground/index";
 import Profile from "views/Profile";
 import { Game } from "views/Game";
 import Quest from "views/Quest";
-import { GenerateChoose } from "views/Generation-choose";
-import { GenerateChoose2 } from "views/Generation-choose2";
-import Dashboard from "views/Dashboard";
+// import Dashboard from "views/Dashboard";
 import Register from "views/Register";
 
 function App() {
@@ -29,11 +27,10 @@ function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/gaming" element={<Game />} />
         <Route path="/quest" element={<Quest />} />
-        <Route path="/generate" element={<Generate />} />
+        {/* <Route path="/generate" element={<Generate />} />
         <Route path="/generateChoose" element={<GenerateChoose />} />
-        <Route path="/generateChoose2" element={<GenerateChoose2 />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/createArt" element={<Dashboard />} />
+        <Route path="/generateChoose2" element={<GenerateChoose2 />} /> */}
+        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
       </Routes>
     </Router>
   );
