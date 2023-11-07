f2df2aa3efc2617bb32b594a09423cebced465fb
Chocominticecream <weihao.kimmers@gmail.com>
2023-11-08 00:21:31 +0800

    made the dashboard responsive
    dashboard should now scale properly with width when the window changes its size

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -79,115 +79,121 @@ const Dashboard = () => {
   }, [result, navigate]);
 
   return (
-    <div className="bg-transparent flex flex-row justify-center w-full">
+    <div className="bg-transparent flex flex-wrap flex-row justify-center w-full">
       <div className="overflow-hidden bg-[url(https://c.animaapp.com/keKAQgUJ/img/group.png)] bg-[100%_100%] w-[1920px] h-[1136.7px] relative">
         <Navbar></Navbar>
-        <div className="relative w-[65%] h-[70%] left-[20%] top-[5%]"
-             style = {{display : "flex"}}>
-          <div className="w-[100%] h-[100%]">
-            <div className="w-[100%] h-[100%] bg-white rounded-[52px]">
-              <div className="absolute w-[523px] top-[58px] left-[72px] font-lapsus font-bold text-black text-[56px] tracking-[0.56px] leading-[normal] whitespace-nowrap">
-                Dashboard
-              </div>
-              <div className="absolute w-[1000px] h-[59px] top-[330px] left-[84px] bg-transparent justify-center">
-                <ProgressBar
-                  bgcolor="#84c455"
-                  progress={currentprofile?.stars}
-                  max={nextLimit}
-                  height={60}
-                />
-              </div>
-              <div className="absolute w-[749px] h-[147px] top-[170px] left-[86px]">
-                <div className="absolute w-[377px] top-0 left-[160px] font-lapsus font-bold text-black text-[60px] tracking-[0] leading-[normal] whitespace-nowrap">
-                  {currentprofile?.firstName}
-                </div>
-                <div className="absolute w-[167px] top-0 left-[578px] font-lapsus font-bold text-black text-[60px] tracking-[0] leading-[normal] whitespace-nowrap">
-                  Level {currentprofile && currentprofile.level}
-                </div>
-                <div className="absolute w-[209px] h-[59px] top-[72px] left-[160px]">
-                  <img
-                    className="absolute w-[68px] h-[65px] top-[-3px] left-[-3px]"
-                    alt="Vector"
-                    src="https://c.animaapp.com/keKAQgUJ/img/vector-1.svg"
-                  />
-                  <div className="w-[127px] top-0 left-[80px] text-[57px] tracking-[0.57px] absolute font-lapsus font-bold text-black leading-[normal] whitespace-nowrap">
-                    {currentprofile?.stars}
-                  </div>
-                </div>
+        <div className="flex flex-col relative w-[65%] h-[75%] left-[20%] top-[5%] flex-wrap bg-white rounded-[52px]">
+          <div className="flex flex-col w-[100%] h-[55%] p-10">
+            <div className="relative w-[100%] h-[20%] top-[0%] font-lapsus font-bold text-black text-[2.5vw] tracking-[0.56px] leading-[normal] whitespace-nowrap">
+              <p>Dashboard</p>
+            </div>
+            {/* profile details */}
+            <div className="flex flex-row relative w-[100%] h-[55%] space-x-5">
+              <div className="relative w-[13%] top-[0%] m-[1%] pl-10">
                 {currentprofile && (
                   <img
-                    className="absolute w-[136px] h-[136px] top-[11px] left-0 rounded-full"
+                    className="rounded-full"
                     alt="Frame"
                     src={defaultPics[currentprofile.profilePic].url}
                   />
                 )}
               </div>
-              <div className="absolute w-[173px] h-[60px] top-[445px] left-[85px]">
-                <div className="absolute w-[171px] top-0 left-0 font-lapsus font-bold text-black text-[49px] tracking-[0.49px] leading-[normal]">
-                  Next up:
+              <div>
+                <div className="flex flex-col space-y-2">
+                  <div className="relative w-[100%] left-[0%] top-[0%] font-lapsus font-bold text-black text-[2.5vw] tracking-[0] leading-[normal] whitespace-nowrap">
+                    {currentprofile?.firstName}
+                  </div>
+                  <div className="flex flex-row space-x-2">
+                    <img
+                      className="relative w-[30%]"
+                      alt="Vector"
+                      src="https://c.animaapp.com/keKAQgUJ/img/vector-1.svg"
+                    />
+                    <div className="relative w-[100%] top-0 left-[0px] text-[2.5vw] tracking-[0.57px] absolute font-lapsus font-bold text-black leading-[normal] whitespace-nowrap">
+                      {currentprofile?.stars}
+                    </div>
+                  </div>
                 </div>
+              </div>
+              <div>
+                <div className="flex justify-end relative w-[100%] font-lapsus font-bold text-black text-[2.5vw] tracking-[0] leading-[normal] whitespace-nowrap">
+                  Level {currentprofile && currentprofile.level}
+                </div>
+              </div>
+            </div>
+
+            <div className="relative w-[100%] h-[20%] justify-center">
+              <ProgressBar
+                bgcolor="#84c455"
+                progress={currentprofile?.stars}
+                max={nextLimit}
+                height={60}
+              />
+            </div>
+
+            <div className="relative w-[100%] h-[5%] ml-[10%]">
+              <div className="font-lapsus font-bold text-black text-[49px] tracking-[0.49px] leading-[normal]">
+                Next up:
               </div>
             </div>
           </div>
           {/*Quest box*/}
           <div
-            className="absolute w-[925px] h-[142px] top-[532px] left-[150px]"
+            className="relative w-[100%] h-[30%] flex flex-col justify-center items-center p-4"
             onClick={() => passQuestParams(quests.category, quests.caption)}
           >
-            <div className="relative w-[929px] h-[142px]">
-              <div className="absolute w-[929px] h-[142px] top-0 left-0">
-                <div className="relative w-[925px] h-[142px]">
-                  <div
-                    className="absolute w-[925px] h-[129px] top-[13px] left-0 bg-[#facd0a] rounded-[24px]"
-                    style={{
-                      backgroundColor:
-                        quests &&
-                        categoryVal[quests.category as keyof typeof categoryVal]
-                          .darkcolor!,
-                    }}
-                  />
-                  <div
-                    className="absolute w-[925px] h-[129px] top-0 left-0 rounded-[24px]"
-                    style={{
-                      backgroundColor:
-                        quests &&
-                        categoryVal[quests.category as keyof typeof categoryVal]
-                          .color!,
-                    }}
-                  />
-                  {/*model icon*/}
-                  <img
-                    id="questImage"
-                    className="absolute w-[95px] h-[61px] top-[35px] left-[17px]"
-                    alt="Frame"
-                    src={
-                      quests &&
-                      categoryVal[quests.category as keyof typeof categoryVal]
-                        .image
-                    }
-                  />
-                  <div className="absolute w-[258px] top-[22px] left-[159px] font-lapsus font-bold text-black text-[31px] tracking-[0] leading-[normal] whitespace-nowrap">
-                    {quests && quests.category}
-                  </div>
-                  <p className="absolute w-[597px] top-[58px] left-[158px] font-lapsus font-normal text-black text-[25px] tracking-[0] leading-[normal]">
-                    {quests && quests.description}
-                  </p>
+            <div
+              className="relative w-[80%] h-[80%] top-[5%] left-0 rounded-t-[20px] font-lapsus p-4"
+              style={{
+                backgroundColor:
+                  quests &&
+                  categoryVal[quests.category as keyof typeof categoryVal]
+                    .color!,
+              }}
+            >
+              <div className="h-[90%] flex flex-row items-center space-x-6 ">
+              {/*model icon*/}
+              <img
+                id="questImage"
+                className="relative w-[10%]"
+                alt="Frame"
+                src={
+                  quests &&
+                  categoryVal[quests.category as keyof typeof categoryVal].image
+                }
+              />
+              <div className="flex flex-col w-[70%] h-[80%] font-lapsus font-normal text-black text-[1.3vw]">
+                <div>
+                  {quests && quests.category}
+                </div>
+                <div className="relative w-[100%] font-lapsus font-normal text-black text-[1.3vw] tracking-[0] leading-[normal]">
+                  {quests && quests.description}
                 </div>
               </div>
               {/*Quest box star reward*/}
-              <div className="absolute w-[142px] h-[62px] top-[43px] left-[772px]">
-                <div className="relative w-[140px] h-[62px] bg-white rounded-[14px]">
+              <div className="flex flex-row space-x-2 items-center relative w-[15%] h-[50%] bg-white rounded-[14px] p-4">
+                <div>
                   <img
-                    className="absolute w-[38px] h-[37px] top-[12px] left-[19px]"
+                    className="relative w-[80%]"
                     alt="Vector"
                     src="https://c.animaapp.com/keKAQgUJ/img/vector.svg"
                   />
-                  <div className="w-[50px] top-[15px] left-[70px] text-[31px] tracking-[0] absolute font-lapsus font-bold text-black leading-[normal] whitespace-nowrap">
+                </div>
+                  <div className="relative w-[10%] text-[2vw] tracking-[0] absolute font-lapsus font-bold text-black leading-[normal] whitespace-nowrap">
                     200
                   </div>
-                </div>
+              </div>
               </div>
             </div>
+            <div
+              className="relative w-[80%] h-[10%] bg-[#facd0a] rounded-b-[15px]"
+              style={{
+                backgroundColor:
+                  quests &&
+                  categoryVal[quests.category as keyof typeof categoryVal]
+                    .darkcolor!,
+              }}
+            ></div>
           </div>
         </div>
       </div>
